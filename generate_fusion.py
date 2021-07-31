bfad444ae1e5e6061b57f9f7d5e50e1befb0e57a
Nguyen Quang Thuc <zthucnguyen99@gmail.com>
2021-07-31 10:30:13 +0700

    Update generate_fusion.py

--- generate_fusion.py
+++ generate_fusion.py
@@ -141,7 +141,7 @@ for data in progressbar(test_loader, max_value=len(test_loader), redirect_stdout
 
         del out_probs
     if (msk.shape[0] == 0):
-        output_mask = np.zeros((msk.shape[-2], msk.shape[-1]))
+        output_mask = np.zeros((msk.shape[-2], msk.shape[-1])).astype(np.uint8)
     elif (output_mask is None):
         original_masks = ((msk[0] > 0.5) * 255).cpu().numpy().astype(np.uint8)
         output_mask = original_masks[target_id][0]
